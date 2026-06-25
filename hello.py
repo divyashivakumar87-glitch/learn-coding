8d826581eab2161898422b5445e0db45d13c6d5b
Divya Shivakumar <dshivakumar@axon.com>
2026-06-25 15:29:43 -0700

    My first commit: add hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+# My first Python program!
+# To run it, type this in your terminal:  python3 hello.py
+
+print("Hello, world!")
+
+# Try changing the text above and running it again.
+# A '#' starts a comment — Python ignores everything after it.
+
+name = "Deepa"          # this stores text in a "variable" called name
+print("My name is", name)
